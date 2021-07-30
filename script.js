9bc86dc292041a927d3ad1a0d128e998997c0c3c
Kirill <Tsarkovkirill@mail.ru>
2021-07-30 13:12:09 +0300

    Unniversary-project

---
+++ script.js
@@ -0,0 +1,29 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minsElement = document.getElementById('mins');
+const secondsElement = document.getElementById('seconds');
+
+const unniversary = "04 Nov 2021";
+
+let countdown = () => {
+	const unniversaryDate = new Date(unniversary);
+	const currentDate = new Date();
+
+	const totalSeconds = (unniversaryDate - currentDate) / 1000;
+
+	const days = Math.floor(totalSeconds / 3600 / 24);
+	const hours = Math.floor(totalSeconds / 3600) % 24;
+	const mins = Math.floor(totalSeconds / 60) % 60;
+	const seconds = Math.floor(totalSeconds) % 60;
+
+	daysElement.innerHTML = days;
+	hoursElement.innerHTML = formatTime(hours);
+	minsElement.innerHTML = formatTime(mins);
+	secondsElement.innerHTML = formatTime(seconds);
+};
+
+let formatTime = (time) => time < 10 ? `0${time}` : time;
+
+countdown();
+
+setInterval(countdown, 1000);
